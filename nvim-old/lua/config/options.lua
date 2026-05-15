aab5c1c69bf8c3e3e0f755bdea66fba65139f843
Byron Seto <byron.seto@stackadapt.com>
2026-05-15 02:52:34 -0600

    removes old nvim

--- nvim-old/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.g.mapleader = ","
-vim.opt.colorcolumn = "80,100"
